e23ccf4b3e895d5bdeec1709928788069f9c4f10
kevaltrivedii <tkeval1610@gmail.com>
2023-01-24 15:34:27 +0530

    practice done

---
+++ user input table.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int  n , i;
+
+    printf("\n\n enter an integer : ");
+    scanf("%d",&n);
+
+    for( i=1; i<=10; ++i){
+        printf("%d x %d = %d \n" , n, i ,n *i);
+    }
+
+
+}
+
+
