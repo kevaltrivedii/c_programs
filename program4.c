b3783bc2b932330c24a75389fb52bf09bd0f0fbc
kevaltrivedii <tkeval1610@gmail.com>
2023-01-26 19:02:49 +0530

    practice completed

---
+++ program4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+
+int length , breadth ,area ;
+
+    printf("\nEnter length of rectangle :");
+    scanf("%d",&length);
+
+    printf("Enter breadth of rectangle :");
+    scanf("%d",&breadth);
+
+    area = length * breadth;
+    printf("\nArea of a rectangle : %d " , area);
+}
+
