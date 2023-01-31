2a20fc7527a2b0266ec33c7b3315b8bed6795941
kevaltrivedii <tkeval1610@gmail.com>
2023-01-31 16:29:39 +0530

    modified program

--- looptable2.c
+++ looptable2.c
@@ -1,34 +1,13 @@
 #include<stdio.h>
 void main(){
 
-int num[] ={3,6,7,9,8,2};
+int numbers[6] ={3,6,7,9,8,2};
 
     
-for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n " , num[0], i ,  num[0] *i);
- }
-
- for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n" , num[1], i ,  num[1] *i);
-
- }
-
-for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n" , num[2], i ,  num[2] *i);
-
-}
-
-for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n" , num[3], i ,  num[3]*i);
-
-}
-
-for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n" , num[4], i , num[4]*i);
-}
-
-for(int i=1; i<=10; i++){
-        printf("%d x %d = %d\n" , num[5], i ,  num[5] *i);
-}
-
+for (int n=1; n<=10; n++) {
+        for (int i=0; i<6; i++) {
+            printf("%d * %d = %d\t| ", numbers[i], n, numbers[i]*n);
+        }
+        printf("\n");
+    }
 }
